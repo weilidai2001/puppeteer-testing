7e1713d9d6a5717b8c456ed9e8161c5a31ea72b3
Weili Dai <weili.dai@turner.com>
2018-07-21 21:37:00 +0100

    Timeout required otherwise the default behaviour is wait until idle which could take a while

--- src/tests/sample-test.test.js
+++ src/tests/sample-test.test.js
@@ -1,11 +1,15 @@
 const puppeteer = require('puppeteer');
 
+const PAGE_LOAD_TIMEOUT = 4000;
+
 let browser;
 let page;
 
 describe('Gumtree()', () => {
     beforeAll(async () => {
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({
+            headless: false,
+        });
     });
 
     afterAll(() => {
@@ -32,9 +36,33 @@ describe('Gumtree()', () => {
                 }
             });
 
-            await page.goto('https://www.gumtree.com/for-sale');
+            try {
+                await page.goto('https://www.gumtree.com/for-sale', {timeout: PAGE_LOAD_TIMEOUT});
+            } catch (e) {}
             expect(isAdSenseCalled).toBe(true);
         }, 10000);
+
+        test('call AdSense server', async () => {
+            await page.setRequestInterception(true);
+            let isAdSenseCalled = false;
+            page.on('request', interceptedRequest => {
+                if (interceptedRequest.url().includes('/afs/ads')) {
+                    isAdSenseCalled = true;
+                    // const [domain, queryParams] = interceptedRequest.url().split('?');
+                    // const queryParamsArray = queryParams.split('&');
+                    // console.log((queryParamsArray))
+                    interceptedRequest.abort();
+                } else {
+                    interceptedRequest.continue();
+                }
+            });
+
+            try {
+                await page.goto('https://www.gumtree.com/for-sale', {timeout: PAGE_LOAD_TIMEOUT});
+            } catch (e) {}
+
+            expect(isAdSenseCalled).toBe(true);
+        }, 20000);
     });
 
     describe('prebid()', () => {
@@ -43,13 +71,18 @@ describe('Gumtree()', () => {
         });
 
         test('prebid version on homepage', async () => {
-            await page.goto('https://www.gumtree.com/');
+            try {
+                await page.goto('https://www.gumtree.com/', {timeout: PAGE_LOAD_TIMEOUT});
+            } catch (e) {}
+
             const prebidVersion = await page.evaluate(`pbjs.version`);
             expect(prebidVersion).toBe('v1.14.0');
         });
 
         test('prebid version on for-sale page', async () => {
-            await page.goto('https://www.gumtree.com/for-sale');
+            try {
+                await page.goto('https://www.gumtree.com/for-sale', {timeout: PAGE_LOAD_TIMEOUT});
+            } catch (e) {}
             const prebidVersion = await page.evaluate(`pbjs.version`);
             expect(prebidVersion).toBe('v1.14.0');
         }, 10000);
