5db4af95cee74f6de6a62910ee9b3e227eb3b4dd
Weili Dai <weili.dai@turner.com>
2018-07-21 21:06:09 +0100

    Clean page for each test

--- src/tests/sample-test.test.js
+++ src/tests/sample-test.test.js
@@ -6,30 +6,19 @@ let page;
 describe('Gumtree()', () => {
     beforeAll(async () => {
         browser = await puppeteer.launch();
-        page = await browser.newPage();
-        await page.setRequestInterception(true);
     });
 
     afterAll(() => {
         browser.close();
     });
 
-    describe('prebid()', () => {
-        test('prebid version on homepage', async () => {
-            await page.goto('https://www.gumtree.com/');
-            const prebidVersion = await page.evaluate(`pbjs.version`);
-            expect(prebidVersion).toBe('v1.14.0');
-        });
-
-        test('prebid version on for-sale page', async () => {
-            await page.goto('https://www.gumtree.com/for-sale');
-            const prebidVersion = await page.evaluate(`pbjs.version`);
-            expect(prebidVersion).toBe('v1.14.0');
-        });
-    });
-
     describe('AdSense()', () => {
+        beforeEach(async () => {
+            page = await browser.newPage();
+        });
+
         test('call AdSense server', async () => {
+            await page.setRequestInterception(true);
             let isAdSenseCalled = false;
             page.on('request', interceptedRequest => {
                 if (interceptedRequest.url().includes('/afs/ads')) {
@@ -45,9 +34,25 @@ describe('Gumtree()', () => {
 
             await page.goto('https://www.gumtree.com/for-sale');
             expect(isAdSenseCalled).toBe(true);
+        }, 10000);
+    });
+
+    describe('prebid()', () => {
+        beforeEach(async () => {
+            page = await browser.newPage();
         });
 
+        test('prebid version on homepage', async () => {
+            await page.goto('https://www.gumtree.com/');
+            const prebidVersion = await page.evaluate(`pbjs.version`);
+            expect(prebidVersion).toBe('v1.14.0');
+        });
 
+        test('prebid version on for-sale page', async () => {
+            await page.goto('https://www.gumtree.com/for-sale');
+            const prebidVersion = await page.evaluate(`pbjs.version`);
+            expect(prebidVersion).toBe('v1.14.0');
+        }, 10000);
     });
 
 });
